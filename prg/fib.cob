b13c1d250e7d7e2f344dec9be0849b76c3c6430d
s1monr3dst0ne07 <s1monr3dst0ne07@outlook.com>
2026-09-24 05:15:54 +0200

    updated fib program with ZEROS name

--- prg/fib.cob
+++ prg/fib.cob
@@ -3,7 +3,7 @@
        working-storage section.
        01 ws-a pic 9(4) value 0001.
        01 ws-b pic 9(4) value 0001.
-       01 ws-c pic 9(4) value 0000.
+       01 ws-c pic 9(4) value ZEROS.
 
        procedure division.
        start.
