e025bd83bed5e97f3e52711f2cc7838a19b8c3b3
s1monr3dst0ne07 <s1monr3dst0ne07@outlook.com>
2026-09-24 05:30:13 +0200

    updated interactive program to be interactive

--- prg/interact.cob
+++ prg/interact.cob
@@ -1,14 +1,19 @@
 
        data division.
        working-storage section.
+       01 ws-name pic a(30) value LOW-VALUES.
+       01 ws-header pic a(50) value "Please enter your name below:".
+
        01 ws-greet.
-           05 ws-header  pic aaaaa value "hello".
+           05 filler pic aaaaa value "Hello".
            05 filler     pic aa    value ", ".
-           05 ws-subject pic a(10) value SPACES.
+           05 ws-subject pic a(30) value SPACES.
            05 filler     pic a     value "!".
        
        procedure division.
        start.
-           move "joe mama" to ws-subject.
+           display ws-header.
+           accept ws-name.
+           move ws-name to ws-subject.
            display ws-greet.
 
